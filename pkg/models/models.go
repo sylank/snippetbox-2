30f1b1bde77ee63614dd6b04f45be4052c5bbd4b
Cuthbert Guerrero <cuthbert.guerrero@gmail.com>
2021-03-16 11:16:35 +0800

    done with 4.4 Designing a Database Model

---
+++ pkg/models/models.go
@@ -0,0 +1,16 @@
+package models
+
+import (
+	"errors"
+	"time"
+)
+
+var ErrNotRecord = errors.New("models: no matching record found")
+
+type Snippet struct {
+	ID      int
+	Title   string
+	Content string
+	Created time.Time
+	Expires time.Time
+}
