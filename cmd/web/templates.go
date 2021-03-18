dd098e1d230e7e39a9d5951b167d31a6763222e7
Cuthbert Guerrero <cuthbert.guerrero@gmail.com>
2021-03-18 20:49:29 +0800

    Done with 8.4 scaling data validations

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -2,17 +2,16 @@ package main
 
 import (
 	"html/template"
-	"net/url"
 	"path/filepath"
 	"time"
 
+	"cubeguerrero.com/snippetbox/pkg/forms"
 	"cubeguerrero.com/snippetbox/pkg/models"
 )
 
 type templateData struct {
 	CurrentYear int
-	FormData    url.Values
-	FormErrors  map[string]string
+	Form        *forms.Form
 	Snippet     *models.Snippet
 	Snippets    []*models.Snippet
 }
