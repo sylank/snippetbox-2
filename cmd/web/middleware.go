7b76f6515c4fd8c53f2abe33cbe03ccfb0e330c4
Cuthbert Guerrero <cuthbert.guerrero@gmail.com>
2021-03-21 13:22:26 +0800

    Done with chapter 12

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"net/http"
+
+	"cubeguerrero.com/snippetbox/pkg/models"
 
 	"github.com/justinas/nosurf"
 )
@@ -58,4 +62,27 @@ func noSurf(next http.Handler) http.Handler {
 	})
 
 	return csrfHandler
+}
+
+func (app *application) authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		exists := app.session.Exists(r, "authenticatedUserID")
+		if !exists {
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		user, err := app.users.Get(app.session.GetInt(r, "authenticatedUserID"))
+		if errors.Is(err, models.ErrNoRecord) || !user.Active {
+			app.session.Remove(r, "authenticatedUserID")
+			next.ServeHTTP(w, r)
+			return
+		} else if err != nil {
+			app.serverError(w, err)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), contextKeyIsAuthenticated, true)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
 }
--- pkg/models/models.go
+++ pkg/models/models.go
@@ -6,7 +6,7 @@ import (
 )
 
 var (
-	ErrNotRecord          = errors.New("models: no matching record found")
+	ErrNoRecord           = errors.New("models: no matching record found")
 	ErrInvalidCredentials = errors.New("models: invalid credentials")
 	ErrDuplicateEmail     = errors.New("models: duplicate email")
 )
